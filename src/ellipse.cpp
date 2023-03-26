52b5a4720396e90160e2152aa709eb06b99dafb2
osawa-koki <github@osawa.dev>
2023-03-26 22:59:03 +0900

    ellipse

---
+++ src/ellipse.cpp
@@ -0,0 +1,62 @@
+#include <png.h>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cmath>
+
+#define OUTPUT_FILE "ellipse.png"
+#define WIDTH 250
+#define HEIGHT 250
+
+int main() {
+  int color = 0xff00ff; // purple color
+  int cx = 125;         // center x coordinate of ellipse
+  int cy = 125;         // center y coordinate of ellipse
+  int rx = 100;         // horizontal radius of ellipse
+  int ry = 50;          // vertical radius of ellipse
+
+  FILE* fp = fopen(OUTPUT_FILE, "wb");
+  if (!fp) {
+    std::cout << "Error: Failed to create file" << std::endl;
+    return -1;
+  }
+
+  png_structp png_ptr;
+  png_infop info_ptr;
+  png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+  info_ptr = png_create_info_struct(png_ptr);
+  png_init_io(png_ptr, fp);
+
+  png_set_IHDR(png_ptr, info_ptr, WIDTH, HEIGHT, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
+         PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+  png_write_info(png_ptr, info_ptr);
+
+  std::vector<png_byte> row(3 * WIDTH);
+  for (int y = 0; y < HEIGHT; ++y) {
+    for (int x = 0; x < WIDTH; ++x) {
+      bool inside_ellipse = pow(x - cx, 2) / pow(rx, 2) + pow(y - cy, 2) / pow(ry, 2) <= 1.0;
+
+      if (inside_ellipse) {
+        png_bytep pixel = &(row[x * 3]);
+        pixel[0] = (color >> 16) & 0xff; // Red
+        pixel[1] = (color >> 8) & 0xff;  // Green
+        pixel[2] = color & 0xff;     // Blue
+      } else {
+        png_bytep pixel = &(row[x * 3]);
+        pixel[0] = 0x00; // Red
+        pixel[1] = 0x00; // Green
+        pixel[2] = 0xff; // Blue
+      }
+    }
+
+    png_write_row(png_ptr, &row[0]);
+  }
+
+  png_write_end(png_ptr, NULL);
+  png_destroy_write_struct(&png_ptr, &info_ptr);
+  fclose(fp);
+
+  std::cout << "Ellipse saved to " << OUTPUT_FILE << std::endl;
+
+  return 0;
+}
